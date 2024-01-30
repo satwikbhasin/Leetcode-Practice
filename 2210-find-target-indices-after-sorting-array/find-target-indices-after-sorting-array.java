25dd24e1a61a959b7abf408a9e21517502a27d68
Satwik Bhasin <satwikbhasin@gmail.com>
2024-01-29 23:25:13 -0500

    Time: 3 ms (45.46%) | Memory: 43.3 MB (15.61%) - LeetSync

--- 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.java
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.java
@@ -2,11 +2,46 @@ class Solution {
     public List<Integer> targetIndices(int[] nums, int target) {
         Arrays.sort(nums);
         List<Integer> list = new ArrayList<>();
-        for (int i = 0; i <nums.length; i++){
-            if(nums[i] == target){
-                list.add(i);
+        int leftmost = -1;
+        int rightmost = -1;
+
+
+        int left = 0;
+        int right = nums.length - 1;
+        while(left<=right){
+            int mid = left + (right - left)/2;
+            if(nums[mid] == target){
+                rightmost = mid;
+                left = mid + 1;
+            } else if(nums[mid] > target){
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+            }
+
+        left = 0;
+        right = nums.length - 1;
+        while(left<=right){
+            int mid = left + (right - left)/2;
+            if(nums[mid] == target){
+                leftmost = mid;
+                right = mid - 1;
+            } else if(nums[mid] > target){
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
+
+        if(leftmost == -1){
+            return list;
+        }
+
+        for( int i = leftmost; i <= rightmost ; i++){
+            list.add(i);
+        }
+
         return list;
     }
 }

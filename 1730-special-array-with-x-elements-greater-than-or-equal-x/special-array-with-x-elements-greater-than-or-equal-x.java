bbf9963ab15fa8bceb1224f625755652c4189686
Satwik Bhasin <satwikbhasin@gmail.com>
2024-01-31 16:22:00 -0500

    Time: 2 ms (41.26%) | Memory: 41.2 MB (51.87%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
@@ -2,22 +2,17 @@ class Solution {
     public int specialArray(int[] nums) {
         Arrays.sort(nums);
         for(int i =1; i <= nums.length; i++){
-            System.out.println("Now checking for " + i);
             int greaterThanEqualToCount = 0;
             int left = 0;
             int right = nums.length - 1;
             while(left<=right){
                 int mid = left + (right-left)/2;
-                System.out.println("At element: " + nums[mid]);
                 if(nums[mid] >= i){
-                    System.out.println("Case 1");
                     right = mid - 1;
                     if((nums.length - mid) > greaterThanEqualToCount){
                     greaterThanEqualToCount = nums.length - mid;
                     }
-                    System.out.println("greaterThanEqualToCount: " + greaterThanEqualToCount);
                 } else {
-                    System.out.println("Case 2");
                     left = mid + 1;
                 }
         }

9905b0d6bf27620cd43209b0b3ecc6eb398a2c12
Satwik Bhasin <satwikbhasin@gmail.com>
2024-01-31 23:30:24 -0500

    Time: 4 ms (52.41%) | Memory: 41 MB (73.79%) - LeetSync

--- 1358-find-positive-integer-solution-for-a-given-equation/find-positive-integer-solution-for-a-given-equation.java
+++ 1358-find-positive-integer-solution-for-a-given-equation/find-positive-integer-solution-for-a-given-equation.java
@@ -11,43 +11,49 @@
 
 class Solution {
     public List<List<Integer>> findSolution(CustomFunction customfunction, int z) {
+
+        // B R U T E F O R C E
+        // List<List<Integer>> ans = new ArrayList<>();
+        // for (int i = 1; i <= 1000; i++){
+        //     if(customfunction.f(i,1) > z){
+        //         return ans;
+        //     }
+        //     for( int j = 1; j <= 1000; j++){
+        //         int sol = customfunction.f(i,j);
+        //         if(sol == z){
+        //             List<Integer> innerList1 = new ArrayList<>();
+        //             innerList1.add(i);
+        //             innerList1.add(j);
+        //             ans.add(innerList1);
+        //         }
+        //         if(sol > z){
+        //             break;
+        //         }
+        //     }
+        // }
+        // return ans;
+
+        // B I N A R Y   S E A R C H
         List<List<Integer>> ans = new ArrayList<>();
         for (int i = 1; i <= 1000; i++){
-            if(customfunction.f(i,1) > z){
-                return ans;
-            }
-            for( int j = 1; j <= 1000; j++){
-                int sol = customfunction.f(i,j);
-                if(sol == z){
-                    List<Integer> innerList1 = new ArrayList<>();
-                    innerList1.add(i);
-                    innerList1.add(j);
-                    ans.add(innerList1);
-                }
+            int left = 1;
+            int right = 1000;
+            while(left<=right){
+                int mid = left + (right-left)/2;
+                int sol = customfunction.f(i,mid);
                 if(sol > z){
+                    right = mid - 1;
+                } else if (sol < z) {
+                    left = mid + 1;
+                } else {
+                    List<Integer> innerList = new ArrayList<>();
+                    innerList.add(i);
+                    innerList.add(mid);
+                    ans.add(innerList);
                     break;
                 }
             }
         }
         return ans;
-        // List<List<Integer>> ans = new ArrayList<>();
-        // for (int i = 1; i <= 1000; i++){
-        //     int left = 1;
-        //     int right = 1000;
-        //     while(left<=right){
-        //         int mid = left + (right-left)/2;
-        //         if(customfunction.f(left,mid) > z){
-        //             right = mid - 1;
-        //         } else if (customfunction.f(left,mid) < z) {
-        //             left = mid + 1;
-        //         } else {
-        //             List<Integer> innerList = new ArrayList<>();
-        //             innerList.add(left);
-        //             innerList.add(mid);
-        //             ans.add(innerList);
-        //         }
-        //     }
-        // }
-        // return ans;
     }
 }

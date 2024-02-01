93d5e5abe8d600baccf6530878228cc1388b213c
Satwik Bhasin <satwikbhasin@gmail.com>
2024-01-31 23:17:07 -0500

    Time: 40 ms (21.03%) | Memory: 41.2 MB (48.97%) - LeetSync

--- 1358-find-positive-integer-solution-for-a-given-equation/find-positive-integer-solution-for-a-given-equation.java
+++ 1358-find-positive-integer-solution-for-a-given-equation/find-positive-integer-solution-for-a-given-equation.java
@@ -12,16 +12,39 @@
 class Solution {
     public List<List<Integer>> findSolution(CustomFunction customfunction, int z) {
         List<List<Integer>> ans = new ArrayList<>();
-        for (int i = 1; i <= 1000; i++){
-            for( int j = 1000; j >= 1; j--){
+        for (int i = 1; i <= 500; i++){
+            for( int j = 500; j >= 1; j--){
                 if(customfunction.f(i,j) == z){
-                    List<Integer> innerList = new ArrayList<>();
-                    innerList.add(i);
-                    innerList.add(j);
-                    ans.add(innerList);
+                    List<Integer> innerList1 = new ArrayList<>();
+                    innerList1.add(i);
+                    innerList1.add(j);
+                    ans.add(innerList1);
+                    // List<Integer> innerList2 = new ArrayList<>();
+                    // innerList2.add(j);
+                    // innerList2.add(i);
+                    // ans.add(innerList2);
                 }
             }
         }
         return ans;
+        // List<List<Integer>> ans = new ArrayList<>();
+        // for (int i = 1; i <= 1000; i++){
+        //     int left = 1;
+        //     int right = 1000;
+        //     while(left<=right){
+        //         int mid = left + (right-left)/2;
+        //         if(customfunction.f(left,mid) > z){
+        //             right = mid - 1;
+        //         } else if (customfunction.f(left,mid) < z) {
+        //             left = mid + 1;
+        //         } else {
+        //             List<Integer> innerList = new ArrayList<>();
+        //             innerList.add(left);
+        //             innerList.add(mid);
+        //             ans.add(innerList);
+        //         }
+        //     }
+        // }
+        // return ans;
     }
 }

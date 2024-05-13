1bacf8437093b9f872d04efc9443a79ef677387c
satwikbhasin <satwikbhasin@gmail.com>
2024-05-13 17:49:48 -0400

    recursion-factorial

---
+++ Playground/RecursionFactorial.java
@@ -0,0 +1,12 @@
+class RecursionFactorial {
+    public static void main(String[] args) {
+        int n = 1;
+        int factorial = factorial(n);
+        System.out.println("Factorial of " + n + " is " + factorial);
+    }
+
+    private static int factorial(int n) {
+        if (n == 0) return 1;
+        return n * factorial(n - 1);
+    }
+}

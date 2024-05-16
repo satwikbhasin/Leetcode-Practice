acb4f3ae3cab1ccd6f7f1bb311e6abc38002ef8b
Satwik Bhasin <satwikbhasin@gmail.com>
2024-05-16 13:05:30 -0400

    Time: 42 ms (58.00%) | Memory: 40.3 MB (49.37%) - LeetSync

---
+++ 526-beautiful-arrangement/beautiful-arrangement.java
@@ -0,0 +1,23 @@
+class Solution {
+    private int dfs(int n, int[] current, int currNumber) {
+        if (currNumber == n) {
+            return 1;
+        }
+
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (current[i - 1] == 0 && (i % (currNumber + 1) == 0 || (currNumber + 1) % i == 0)) {
+                current[i - 1] = 1;
+                count += dfs(n, current, currNumber + 1);
+                current[i - 1] = 0;
+            }
+        }
+
+        return count;
+    }
+
+    public int countArrangement(int n) {
+        return dfs(n, new int[n + 1], 0);
+    }
+}

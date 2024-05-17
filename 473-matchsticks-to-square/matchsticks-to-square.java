167c55cda780fd27082c910bb84d2252fdc84fcf
Satwik Bhasin <satwikbhasin@gmail.com>
2024-05-16 20:39:53 -0400

    Time: 104 ms (63.32%) | Memory: 40.5 MB (99.58%) - LeetSync

--- 473-matchsticks-to-square/matchsticks-to-square.java
+++ 473-matchsticks-to-square/matchsticks-to-square.java
@@ -15,7 +15,7 @@ class Solution {
             }
             // Optimization: If this matchstick can't be placed in the current empty side,
             // there's no point in trying to place it in the next empty side.
-            if (sides[i] == 0) break;
+            // if (sides[i] == 0) break;
         }
 
         return false;

952ea394c285b253149cefdd5c8d18ccc85c3464
Satwik Bhasin <satwikbhasin@gmail.com>
2024-05-17 00:45:05 -0400

    Time: 3 ms (85.41%) | Memory: 49.7 MB (25.71%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,18 +1,14 @@
 class Solution {
 
     private void dfs(char[][] grid, int x, int y) {
-        // Check boundary conditions
         if (x < 0 || y < 0 || x >= grid.length || y >= grid[0].length) {
             return;
         }
-        // If the cell is water or already visited, return
         if (grid[x][y] == '0') {
             return;
         }
-        // Mark the cell as visited by setting it to '0'
         grid[x][y] = '0';
 
-        // Recursively visit all adjacent cells
         dfs(grid, x + 1, y);
         dfs(grid, x - 1, y);
         dfs(grid, x, y + 1);

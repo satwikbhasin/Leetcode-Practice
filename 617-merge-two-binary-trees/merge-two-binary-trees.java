a33a23b7534fae9ce96f3910ede11378a0d3e939
Satwik Bhasin <satwikbhasin@gmail.com>
2024-05-17 23:39:33 -0400

    Time: 4 ms (17.81%) | Memory: 44.4 MB (92.20%) - LeetSync

--- 617-merge-two-binary-trees/merge-two-binary-trees.java
+++ 617-merge-two-binary-trees/merge-two-binary-trees.java
@@ -1,40 +1,57 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
-        TreeNode newRoot = new TreeNode();
+        if (root1 == null && root2 == null)
+            return null;
+        if (root1 == null || root2 == null)
+            return root1 == null ? root2 : root1;
 
-        if(root1 == null && root2 == null)
-            newRoot = null;
-        else if(root1 == null && root2 != null){
-            newRoot.val =  root2.val;
-            newRoot.left = mergeTrees(null, root2.left);
-            newRoot.right = mergeTrees(null, root2.right);        
+        TreeNode newTree = new TreeNode(root1.val + root2.val);
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root1);
+        queue.add(root2);
+        queue.add(newTree);
+
+        while (!queue.isEmpty()) {
+            TreeNode firstTreeNode = queue.remove();
+            TreeNode secondTreeNode = queue.remove();
+            TreeNode newTreeNode = queue.remove();
+
+            if (firstTreeNode.left != null || secondTreeNode.left != null) {
+                TreeNode firstTreeLeftNode = firstTreeNode.left != null ? firstTreeNode.left : new TreeNode();
+                TreeNode secondTreeLeftNode = secondTreeNode.left != null ? secondTreeNode.left : new TreeNode();
+                newTreeNode.left = new TreeNode(firstTreeLeftNode.val + secondTreeLeftNode.val);
+                queue.add(firstTreeLeftNode);
+                queue.add(secondTreeLeftNode);
+                queue.add(newTreeNode.left);
             }
-        else if(root1 != null && root2 == null){
-            newRoot.val = root1.val;
-            newRoot.left = mergeTrees(root1.left, null);
-            newRoot.right = mergeTrees(root1.right, null); 
-        }
-        else{
-            newRoot.val = root1.val + root2.val;
-            newRoot.left = mergeTrees(root1.left, root2.left);
-            newRoot.right = mergeTrees(root1.right, root2.right); 
+
+            if (firstTreeNode.right != null || secondTreeNode.right != null) {
+                TreeNode firstTreeRightNode = firstTreeNode.right != null ? firstTreeNode.right : new TreeNode();
+                TreeNode secondTreeRightNode = secondTreeNode.right != null ? secondTreeNode.right : new TreeNode();
+                newTreeNode.right = new TreeNode(firstTreeRightNode.val + secondTreeRightNode.val);
+                queue.add(firstTreeRightNode);
+                queue.add(secondTreeRightNode);
+                queue.add(newTreeNode.right);
+            }
+
         }
 
-        return newRoot;
+        return newTree;
     }
 }

93eae405e364f1be33f213f675b92b8114e8218c
Satwik Bhasin <satwikbhasin@gmail.com>
2024-05-19 18:50:40 -0400

    Time: 6 ms (56.71%) | Memory: 46.2 MB (13.84%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -2,48 +2,41 @@ class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         int[] courseSchedule = new int[numCourses];
         Map<Integer, List<Integer>> graph = new HashMap<>();
-        Map<Integer, Integer> indegreeCounts = new HashMap<>();
+        int[] indegreeCounts = new int[numCourses];
         Queue<Integer> queue = new LinkedList<>();
 
         for (int i = 0; i < numCourses; i++) {
             graph.put(i, new ArrayList<>());
-            indegreeCounts.put(i, 0);
+            indegreeCounts[i] = 0;
         }
 
         for (int[] prereq : prerequisites) {
             graph.get(prereq[1]).add(prereq[0]);
-            indegreeCounts.put(prereq[0], indegreeCounts.get(prereq[0]) + 1);
+            indegreeCounts[prereq[0]]++;
         }
 
-        for(int i = 0; i < numCourses; i++){
-            if(indegreeCounts.get(i) == 0){
+        for (int i = 0; i < numCourses; i++) {
+            if (indegreeCounts[i] == 0) {
                 queue.add(i);
             }
         }
 
-        if(queue.isEmpty()){
-            return new int[0]; 
+        if (queue.isEmpty()) {
+            return new int[0];
         }
 
         int courseNumber = 0;
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             int currentCourse = queue.remove();
-            courseSchedule[courseNumber] = currentCourse;
-            courseNumber++;
-            for( int neighbor : graph.get(currentCourse)){
-                indegreeCounts.put(neighbor, indegreeCounts.get(neighbor) - 1);
-                if(indegreeCounts.get(neighbor) == 0){
+            courseSchedule[courseNumber++] = currentCourse;
+            for (int neighbor : graph.get(currentCourse)) {
+                indegreeCounts[neighbor]--;
+                if (indegreeCounts[neighbor] == 0) {
                     queue.add(neighbor);
                 }
             }
         }
 
-        for (int count : indegreeCounts.values()) {
-            if (count != 0) {
-                return new int[0];
-            }
-        }
-
-        return courseSchedule;
+        return courseNumber == numCourses ? courseSchedule : new int[0];
     }
 }

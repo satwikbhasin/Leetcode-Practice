02286fc9e826ba47812bacdee68a759293c6edba
Satwik Bhasin <satwikbhasin@gmail.com>
2024-05-20 02:09:24 -0400

    Time: 16 ms (68.62%) | Memory: 45.4 MB (60.21%) - LeetSync

---
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -0,0 +1,43 @@
+class Solution {
+
+    private static int[][] directions = new int[][] { { 1, 1 }, { 1, 0 }, { 0, 1 }, { -1, -1 }, { -1, 1 },
+            { 1, -1 }, { 0, -1 }, { -1, 0 } };
+
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        if (grid[0][0] != 0)
+            return -1;
+
+        Queue<int[]> queue = new LinkedList<>();
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+
+        queue.add(new int[] { 0, 0 });
+        visited[0][0] = true;
+
+        int clearPathLength = 1;
+
+        while (!queue.isEmpty()) {
+            int currentBreadth = queue.size();
+            for (int i = 0; i < currentBreadth; i++) {
+                int[] cell = queue.remove();
+                int x = cell[0];
+                int y = cell[1];
+
+                if (x == grid.length - 1 && y == grid[0].length - 1)
+                    return clearPathLength;
+
+                for (int j = 0; j < 8; j++) {
+                    int newX = x + directions[j][0];
+                    int newY = y + directions[j][1];
+
+                    if (newX >= 0 && newY >= 0 && newX < grid.length && newY < grid[0].length && !visited[newX][newY]
+                            && grid[newX][newY] == 0) {
+                        queue.add(new int[] { newX, newY });
+                        visited[newX][newY] = true;
+                    }
+                }
+            }
+            clearPathLength++;
+        }
+        return -1;
+    }
+}

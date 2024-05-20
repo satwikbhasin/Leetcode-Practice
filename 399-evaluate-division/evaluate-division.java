3c62f9831da317145a4e34d58efb9ea6690cb010
Satwik Bhasin <satwikbhasin@gmail.com>
2024-05-20 13:07:08 -0400

    Time: 1 ms (98.60%) | Memory: 42 MB (52.59%) - LeetSync

--- 399-evaluate-division/evaluate-division.java
+++ 399-evaluate-division/evaluate-division.java
@@ -1,80 +1,12 @@
-// class Solution {
-//     private double dfs(Map<String, HashMap<String, Double>> graph, String source, String target, double productTillNow,
-//             HashSet<String> visited) {
-//         double ans = -1.0;
-
-//         Map<String, Double> neighbors = graph.get(source);
-//         if (neighbors.containsKey(target))
-//             ans = productTillNow * neighbors.get(target);
-//         else {
-//             for (Map.Entry<String, Double> neighbor : neighbors.entrySet()) {
-//                 String neighborName = neighbor.getKey();
-//                 Double neighborValue = neighbor.getValue();
-//                 if (!visited.contains(neighborName)) {
-//                     visited.add(neighborName);
-//                     ans = dfs(graph, neighborName, target, productTillNow * neighborValue, visited);
-//                     if (ans == -1.0) {
-//                         break;
-//                     }
-//                     visited.remove(neighborName);
-//                 }
-//             }
-//         }
-
-//         return ans;
-//     }
-
-//     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-//         Map<String, HashMap<String, Double>> graph = new HashMap<>();
-//         for (int i = 0; i < equations.size(); i++) {
-//             List<String> equation = equations.get(i);
-//             String dividend = equation.get(0);
-//             String divisor = equation.get(1);
-//             double quotient = values[i];
-
-//             if (!graph.containsKey(dividend)) {
-//                 graph.put(dividend, new HashMap<String, Double>());
-//             }
-//             if (!graph.containsKey(divisor)) {
-//                 graph.put(divisor, new HashMap<String, Double>());
-//             }
-
-//             graph.get(dividend).put(divisor, quotient);
-//             graph.get(divisor).put(dividend, 1 / quotient);
-//         }
-
-//         double[] results = new double[queries.size()];
-//         for (int i = 0; i < queries.size(); i++) {
-//             List<String> query = queries.get(i);
-//             String source = query.get(0);
-//             String target = query.get(1);
-
-//             if (!graph.containsKey(source) || !graph.containsKey(target))
-//                 results[i] = -1.0;
-//             else if (source.equals(target))
-//                 results[i] = 1.0;
-//             else {
-//                 HashSet<String> visited = new HashSet<String>();
-//                 visited.add(source);
-//                 results[i] = dfs(graph, source, target, 1.0, visited);
-//             }
-//         }
-
-//         return results;
-//     }
-// }
-
-import java.util.*;
-
 class Solution {
     private double dfs(Map<String, HashMap<String, Double>> graph, String source, String target, double productTillNow,
-                       HashSet<String> visited) {
+            HashSet<String> visited) {
         double ans = -1.0;
 
         Map<String, Double> neighbors = graph.get(source);
-        if (neighbors.containsKey(target)) {
+        if (neighbors.containsKey(target))
             ans = productTillNow * neighbors.get(target);
-        } else {
+        else {
             for (Map.Entry<String, Double> neighbor : neighbors.entrySet()) {
                 String neighborName = neighbor.getKey();
                 Double neighborValue = neighbor.getValue();
@@ -101,10 +33,10 @@ class Solution {
             double quotient = values[i];
 
             if (!graph.containsKey(dividend)) {
-                graph.put(dividend, new HashMap<>());
+                graph.put(dividend, new HashMap<String, Double>());
             }
             if (!graph.containsKey(divisor)) {
-                graph.put(divisor, new HashMap<>());
+                graph.put(divisor, new HashMap<String, Double>());
             }
 
             graph.get(dividend).put(divisor, quotient);
@@ -117,12 +49,12 @@ class Solution {
             String source = query.get(0);
             String target = query.get(1);
 
-            if (!graph.containsKey(source) || !graph.containsKey(target)) {
+            if (!graph.containsKey(source) || !graph.containsKey(target))
                 results[i] = -1.0;
-            } else if (source.equals(target)) {
+            else if (source.equals(target))
                 results[i] = 1.0;
-            } else {
-                HashSet<String> visited = new HashSet<>();
+            else {
+                HashSet<String> visited = new HashSet<String>();
                 visited.add(source);
                 results[i] = dfs(graph, source, target, 1.0, visited);
             }

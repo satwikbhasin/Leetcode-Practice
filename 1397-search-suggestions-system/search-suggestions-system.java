75d4dd7a634cc480f0aa7761be7ba14fdb11eb63
Satwik Bhasin <satwikbhasin@gmail.com>
2024-05-21 16:21:53 -0400

    Time: 37 ms (39.39%) | Memory: 47.7 MB (50.97%) - LeetSync

--- 1397-search-suggestions-system/search-suggestions-system.java
+++ 1397-search-suggestions-system/search-suggestions-system.java
@@ -1,42 +1,21 @@
 class Solution {
-
-    private int findFirstOccuranceOfPrefix(String[] products, String searchWord, int i, int left, int right){
-        int firstOccurance = -1;
-        while (left <= right) {
-                int mid = left + (right - left) / 2;
-                int len = Math.min((i), products[mid].length() - 1);
-                if (products[mid].substring(0, len + 1).compareTo(searchWord.substring(0, i + 1)) > 0) {
-                    right = mid - 1;
-                } else if (products[mid].substring(0, len + 1).compareTo(searchWord.substring(0, i + 1)) < 0) {
-                    left = mid + 1;
-                } else {
-                    firstOccurance = mid;
-                    right = mid - 1;
+    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+        List<List<String>> results = new ArrayList<>();
+        for (int i = 0; i < searchWord.length(); i++) {
+            String toSearch = searchWord.substring(0, i + 1);
+            List<String> result = new ArrayList<>();
+            for (String product : products) {
+                if (product.startsWith(toSearch)) {
+                    result.add(product);
                 }
             }
-        return firstOccurance;
-    }
-
-    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
-        Arrays.sort(products);
-        List<List<String>> outerList = new ArrayList<>();
-
-        for (int i = 0; i < searchWord.length(); i++) {
-            List<String> innerList = new ArrayList<>();
-            int left = 0;
-            int right = products.length - 1;
-
-            int firstOccurance = findFirstOccuranceOfPrefix(products, searchWord, i, left, right);
-            
-            if(firstOccurance != -1){
-                innerList.add(products[firstOccurance]);
-                if((firstOccurance + 1) < products.length && products[firstOccurance+1].startsWith(searchWord.substring(0, i + 1)))
-                    innerList.add(products[firstOccurance+1]);
-                if((firstOccurance + 2) < products.length && products[firstOccurance+2].startsWith(searchWord.substring(0, i + 1)))
-                    innerList.add(products[firstOccurance+2]);
-            }   
-                outerList.add(innerList);
+            Collections.sort(result);
+            if (result.size() > 3) {
+                results.add(result.subList(0, 3));
+            } else {
+                results.add(result);
             }
-        return outerList;
+        }
+        return results;
     }
 }

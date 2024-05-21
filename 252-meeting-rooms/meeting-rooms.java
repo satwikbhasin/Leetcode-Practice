3f7003cbe4b8e5bf5a3464095d14459305c84943
Satwik Bhasin <satwikbhasin@gmail.com>
2024-05-21 00:58:10 -0400

    Time: 6 ms (79.62%) | Memory: 45.9 MB (42.89%) - LeetSync

--- 252-meeting-rooms/meeting-rooms.java
+++ 252-meeting-rooms/meeting-rooms.java
@@ -2,10 +2,6 @@ class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> (a[0] - b[0]));
 
-        for (int[] interval : intervals) {
-            System.out.println(interval[0] + ", " + interval[1]);
-        }
-
         for (int i = 1; i < intervals.length; i++) {
             if (intervals[i][0] < intervals[i - 1][1])
                 return false;

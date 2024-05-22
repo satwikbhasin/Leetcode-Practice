4fc83aeda4beef20f2ff5cd172b7a3aec43c35e8
Satwik Bhasin <satwikbhasin@gmail.com>
2024-05-22 16:44:41 -0400

    Time: 4 ms (96.25%) | Memory: 45.5 MB (88.50%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,36 +1,49 @@
 class Solution {
-    public class Element {
-        int val;
-        int distanceToX;
-        int index;
-
-        public Element(int val, int x, int index) {
-            this.val = val;
-            this.distanceToX = Math.abs(val - x);
-            this.index = index;
-        }
+    public int getClosenessToX(int var, int x) {
+        return Math.abs(var - x);
     }
 
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        PriorityQueue<Element> pq = new PriorityQueue<>((b, a) -> {
-            if (a.distanceToX == b.distanceToX)
-                return a.val - b.val;
-            else
-                return a.distanceToX - b.distanceToX;
-        });
+        int left = 0;
+        int right = arr.length - 1;
 
-        for (int i = 0; i < arr.length; i++) {
-            pq.add(new Element(arr[i], x, i));
-            if (pq.size() > k)
-                pq.poll();
+        // Binary search to find the position of x or the closest position
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] > x) {
+                right = mid - 1;
+            } else if (arr[mid] < x) {
+                left = mid + 1;
+            } else {
+                left = mid;
+                break;
+            }
+        }
+
+        int posOfX = left;
+        left = posOfX - 1;
+        right = posOfX;
+
+        while (right - left - 1 < k) {
+            if (left == -1) {
+                right += 1;
+                continue;
+            } else if (right == arr.length) {
+                left -= 1;
+                continue;
+            }
+
+            if (getClosenessToX(arr[left], x) <= getClosenessToX(arr[right], x)) {
+                left -= 1;
+            } else {
+                right += 1;
+            }
         }
 
         List<Integer> result = new ArrayList<>();
-        for (int i = 0; i < k; i++) {
-            result.add(pq.poll().val);
+        for (int i = left + 1; i < right; i++) {
+            result.add(arr[i]);
         }
-
-        Collections.sort(result);
 
         return result;
     }

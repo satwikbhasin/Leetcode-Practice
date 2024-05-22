e56418901945c3580307b6a4468e3006f72c574f
Satwik Bhasin <satwikbhasin@gmail.com>
2024-05-22 12:34:08 -0400

    Time: 20 ms (28.43%) | Memory: 57.7 MB (37.50%) - LeetSync

--- 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.java
+++ 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.java
@@ -1,39 +1,3 @@
-// class Solution {
-//     public int furthestBuilding(int[] heights, int bricks, int ladders) {
-//         PriorityQueue<Integer> ladderUsedFor = new PriorityQueue<>();
-//         int furthestBuilding = 0;
-
-//         for (int i = 0; i < heights.length - 1; i++) {
-//             int heightDiff = heights[i] - heights[i + 1];
-//             if (heightDiff < 0) {
-//                 if (ladders > 0) {
-//                     ladders--;
-//                     ladderUsedFor.add(heightDiff);
-//                     furthestBuilding++;
-//                 } else if (ladders == 0) {
-//                     if (!ladderUsedFor.isEmpty() && heightDiff > ladderUsedFor.peek()
-//                             && ladderUsedFor.peek() <= bricks) {
-//                         bricks -= ladderUsedFor.poll();
-//                         ladderUsedFor.add(heightDiff);
-//                         furthestBuilding++;
-//                     } else {
-//                         if (bricks - heightDiff >= 0) {
-//                             bricks -= heightDiff;
-//                             furthestBuilding++;
-//                         } else
-//                             break;
-//                     }
-//                 }
-//             } else {
-//                 furthestBuilding++;
-//             }
-//         }
-//         return furthestBuilding;
-//     }
-// }
-
-import java.util.PriorityQueue;
-
 class Solution {
     public int furthestBuilding(int[] heights, int bricks, int ladders) {
         PriorityQueue<Integer> ladderUsedFor = new PriorityQueue<>();
@@ -43,18 +7,15 @@ class Solution {
             int heightDiff = heights[i + 1] - heights[i];
             if (heightDiff > 0) {
                 ladderUsedFor.add(heightDiff);
-                
-                if (ladderUsedFor.size() > ladders) {
-                    bricks -= ladderUsedFor.poll();
-                }
 
-                if (bricks < 0) {
-                    return furthestBuilding;
-                }
+                if (ladderUsedFor.size() > ladders)
+                    bricks -= ladderUsedFor.poll();
+
+                if (bricks < 0)
+                    return i;
             }
             furthestBuilding = i + 1;
         }
-
         return furthestBuilding;
     }
 }

eaaed2a46dcca898e3d151ff3ee1c24603906037
Satwik Bhasin <satwikbhasin@gmail.com>
2024-05-27 01:46:50 -0400

    Time: 1 ms (100.00%) | Memory: 46.1 MB (16.61%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,40 +1,39 @@
 class Solution {
-    private int findK(int[] nums1, int[] nums2, int aStart, int aEnd, int bStart, int bEnd, int k) {
-        if (aEnd < aStart) {
-            return nums2[k - aStart];
-        }
-
-        if (bEnd < bStart) {
-            return nums1[k - bStart];
-        }
-
-        int aIndex = (aStart + aEnd) / 2, bIndex = (bStart + bEnd) / 2;
-        int aValue = nums1[aIndex], bValue = nums2[bIndex];
-
-        if (aIndex + bIndex < k) {
-            if (aValue > bValue) {
-                return findK(nums1, nums2, aStart, aEnd, bIndex + 1, bEnd, k);
-            } else {
-                return findK(nums1, nums2, aIndex + 1, aEnd, bStart, bEnd, k);
-            }
-        } else {
-            if (aValue > bValue) {
-                return findK(nums1, nums2, aStart, aIndex - 1, bStart, bEnd, k);
-            } else {
-                return findK(nums1, nums2, aStart, aEnd, bStart, bIndex - 1, k);
-            }
-        }
-    }
-
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
         int n1 = nums1.length;
         int n2 = nums2.length;
-        int n = n1 + n2;
-        if (n % 2 == 0) {
-            return (double) (findK(nums1, nums2, 0, n1 - 1, 0, n2 - 1, n / 2)
-                    + findK(nums1, nums2, 0, n1 - 1, 0, n2 - 1, n / 2 - 1)) / 2;
-        } else {
-            return findK(nums1, nums2, 0, n1 - 1, 0, n2 - 1, n / 2);
+
+        int low = 0;
+        int high = n1;
+
+        while (low <= high) {
+
+            int partition1 = (low + high) / 2;
+            int partition2 = (n1 + n2 + 1) / 2 - partition1;
+
+            int maxLeft1 = partition1 == 0 ? Integer.MIN_VALUE : nums1[partition1 - 1];
+            int minRight1 = partition1 == n1 ? Integer.MAX_VALUE : nums1[partition1];
+
+            int maxLeft2 = partition2 == 0 ? Integer.MIN_VALUE : nums2[partition2 - 1];
+            int minRight2 = partition2 == n2 ? Integer.MAX_VALUE : nums2[partition2];
+
+            if (maxLeft1 <= minRight2 && maxLeft2 <= minRight1) {
+                if ((n1 + n2) % 2 == 0) {
+                    return ((double) Math.max(maxLeft1, maxLeft2) + Math.min(minRight2, minRight1)) / 2;
+                } else {
+                    return (double)Math.max(maxLeft1, maxLeft2);
+                }
+            } else if (maxLeft1 > minRight2) {
+                high = partition1 - 1;
+            } else {
+                low = partition1 + 1;
+            }
         }
+
+        return -1.0;
     }
 }

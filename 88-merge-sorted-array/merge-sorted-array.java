ee835552a2e42f2f97f7347a60f06cb592ba1734
Satwik Bhasin <satwikbhasin@gmail.com>
2024-05-27 17:43:19 -0400

    Time: 0 ms (100.00%) | Memory: 42 MB (67.41%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,24 +1,27 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int ptr1 = m - 1, ptr2 = n - 1;
-        int insertIndex = m + n - 1;
+        int ptr1 = m - 1;
+        int ptr2 = n - 1;
 
-        while (ptr1 >= 0 && ptr2 >= 0) {
-            if (nums1[ptr1] > nums2[ptr2]) {
-                nums1[insertIndex] = nums1[ptr1];
-                insertIndex--;
-                ptr1--;
-            } else {
-                nums1[insertIndex] = nums2[ptr2];
-                insertIndex--;
-                ptr2--;
+        int index = nums1.length - 1;
+
+        while (ptr1 >= 0 || ptr2 >= 0) {
+            if (ptr1 >= 0 && ptr2 >= 0) {
+                if (nums1[ptr1] > nums2[ptr2]) {
+                    nums1[index--] = nums1[ptr1--];
+                } else {
+                    nums1[index--] = nums2[ptr2--];
+                }
+
             }
-        }
 
-        while (ptr2 >= 0) {
-            nums1[insertIndex] = nums2[ptr2];
-            insertIndex--;
-            ptr2--;
+            if (ptr2 < 0 && ptr1 >= 0) {
+                nums1[index--] = nums1[ptr1--];
+            }
+
+            if (ptr1 < 0 && ptr2 >= 0) {
+                nums1[index--] = nums2[ptr2--];
+            }
         }
 
     }

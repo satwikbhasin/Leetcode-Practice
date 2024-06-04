ed7a01c5e79c793cb46c10166576dd9a3dd7f608
Satwik Bhasin <satwikbhasin@gmail.com>
2024-06-04 02:38:50 -0400

    Time: 3 ms (73.23%) | Memory: 43 MB (64.52%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,26 @@
+class Solution {
+    public static boolean isLetterOrDigit(char c) {
+        return Character.isLetter(c) || Character.isDigit(c);
+    }
+
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left <= right) {
+            while (left <= right && !isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while (right >= left && !isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+            if (left <= right && Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}

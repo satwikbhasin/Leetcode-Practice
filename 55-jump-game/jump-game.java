0aa3eac3da13f7e6893db5a163a608deb1100e42
Satwik Bhasin <satwikbhasin@gmail.com>
2024-06-11 19:39:31 -0400

    Time: 1 ms (99.97%) | Memory: 45.4 MB (61.76%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,11 +1,39 @@
+// O(n^2) Time, O(n) Space
+// class Solution {
+//     private boolean canJumpFromCurrentPos(int pos, int[] nums, int[] memo) {
+//         if (memo[pos] != -1) {
+//             return memo[pos] == 1 ? true : false;
+//         }
+
+//         int furthestJumpIndex = Math.min(pos + nums[pos], nums.length - 1);
+//         for (int i = pos + 1; i <= furthestJumpIndex; i++) {
+//             if(canJumpFromCurrentPos(i, nums, memo)){
+//                 memo[pos] = 1;
+//                 return true;
+//             }
+//         }
+
+//         memo[pos] = 0;
+//         return false;
+//     }
+
+//     public boolean canJump(int[] nums) {
+//         int[] memo = new int[nums.length];
+//         Arrays.fill(memo, -1);
+//         memo[nums.length - 1] = 1;
+//         return canJumpFromCurrentPos(0, nums, memo);
+//     }
+// }
+
+// O(n) Time, O(1) Space
 class Solution {
     public boolean canJump(int[] nums) {
-        int lastPos = nums.length - 1;
+        int lastGoodPos = nums.length - 1;
         for (int i = nums.length - 1; i >= 0; i--) {
-            if (i + nums[i] >= lastPos) {
-                lastPos = i;
+            if (i + nums[i] >= lastGoodPos) {
+                lastGoodPos = i;
             }
         }
-        return lastPos == 0;
+        return lastGoodPos == 0;
     }
 }

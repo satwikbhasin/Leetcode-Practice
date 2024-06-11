d938aad1178f5dce42d5719fcdede067adf50e89
Satwik Bhasin <satwikbhasin@gmail.com>
2024-06-11 18:18:34 -0400

    Time: 0 ms (100.00%) | Memory: 57.2 MB (67.09%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,12 +1,33 @@
+// O(n) Time Complexity, O(1) Space Complexity
 class Solution {
     public void rotate(int[] nums, int k) {
-        int[] copy = new int[nums.length];
         k = k % nums.length;
-        for (int i = 0; i < nums.length; i++) {
-            copy[(i + k) % nums.length] = nums[i];
-        }
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = copy[i];
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start <= end) {
+            int temp = nums[end];
+            nums[end] = nums[start];
+            nums[start] = temp;
+            start++;
+            end--;
         }
     }
 }
+
+// O(n) Time Complexity, O(n) Space Complexity
+// class Solution {
+// public void rotate(int[] nums, int k) {
+// int[] copy = new int[nums.length];
+// k = k % nums.length;
+// for (int i = 0; i < nums.length; i++) {
+// copy[(i + k) % nums.length] = nums[i];
+// }
+// for (int i = 0; i < nums.length; i++) {
+// nums[i] = copy[i];
+// }
+// }
+// }

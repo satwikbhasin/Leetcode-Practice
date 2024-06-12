b660892e16b3cbce577ac18e4344a29ff5066561
Satwik Bhasin <satwikbhasin@gmail.com>
2024-06-12 19:01:31 -0400

    Time: 0 ms (100.00%) | Memory: 41.1 MB (81.33%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,18 +1,53 @@
+// O(n * log m) Time, O(1) Space
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-
-        String comparatorString = strs[0];
-        int prefixEndIndex = comparatorString.length();
+        int minLength = Integer.MAX_VALUE;
         for (String str : strs) {
-            prefixEndIndex = Math.min(prefixEndIndex, str.length());
-            for (int i = 0; i < prefixEndIndex; i++) {
-                if (str.charAt(i) != comparatorString.charAt(i)) {
-                    prefixEndIndex = i;
-                    break;
-                }
+            minLength = Math.min(minLength, str.length());
+        }
+
+        int min = 0;
+        int max = minLength;
+        while (min <= max) {
+            int mid = min + (max - min) / 2;
+            if (isCommonForAll(strs, mid)) {
+                min = mid + 1;
+            } else {
+                max = mid - 1;
             }
         }
 
-        return prefixEndIndex == -1 ? "" : comparatorString.substring(0, prefixEndIndex);
+        return strs[0].substring(0, max);
+    }
+
+    private boolean isCommonForAll(String[] strs, int len) {
+        String prefixCandidate = strs[0].substring(0, len);
+        for (String str : strs) {
+            if (!str.startsWith(prefixCandidate)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
+
+// O(n * m) Time, O(1) Space
+// class Solution {
+// public String longestCommonPrefix(String[] strs) {
+
+// String comparatorString = strs[0];
+// int prefixEndIndex = comparatorString.length();
+// for (String str : strs) {
+// prefixEndIndex = Math.min(prefixEndIndex, str.length());
+// for (int i = 0; i < prefixEndIndex; i++) {
+// if (str.charAt(i) != comparatorString.charAt(i)) {
+// prefixEndIndex = i;
+// break;
+// }
+// }
+// }
+
+// return prefixEndIndex == -1 ? "" : comparatorString.substring(0,
+// prefixEndIndex);
+// }
+// }

5a5f9d91e06bcb3b784e4c6922062142986f1292
Satwik Bhasin <satwikbhasin@gmail.com>
2024-06-12 18:34:50 -0400

    Time: 0 ms (100.00%) | Memory: 41.4 MB (83.57%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,3 +1,5 @@
+// O(n) Time where n is the length of substring starting from the first character of the last word to the end of the original string
+// O(1) Space
 class Solution {
     public int lengthOfLastWord(String s) {
         int length = 0;

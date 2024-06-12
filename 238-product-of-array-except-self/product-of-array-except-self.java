dbe9dc06794dc584527b8bd3778e866d7e7fbd82
Satwik Bhasin <satwikbhasin@gmail.com>
2024-06-11 23:05:35 -0400

    Time: 2 ms (88.88%) | Memory: 55 MB (82.83%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,35 +1,56 @@
+// O(n) Time, O(1) Space, Without using division
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int zeroCount = 0;
-        int product = 1;
         int[] solution = new int[nums.length];
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                zeroCount++;
-            } else {
-                product *= nums[i];
-            }
+        solution[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            solution[i] = solution[i - 1] * nums[i - 1];
         }
 
-        if (zeroCount > 1) {
-            return solution;
-        }
-
-        if (zeroCount == 1) {
-            for (int i = 0; i < nums.length; i++) {
-                if (nums[i] == 0) {
-                    solution[i] = product;
-                    return solution;
-                }
-            }
-        }
-
-        for (int i = 0; i < nums.length; i++) {
-            solution[i] = product / nums[i];
+        int rightProduct = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            solution[i] = solution[i] * rightProduct;
+            rightProduct *= nums[i];
         }
 
         return solution;
-
     }
 }
+
+// O(n) Time, O(1) Space, Using division
+// class Solution {
+// public int[] productExceptSelf(int[] nums) {
+// int zeroCount = 0;
+// int product = 1;
+// int[] solution = new int[nums.length];
+
+// for (int i = 0; i < nums.length; i++) {
+// if (nums[i] == 0) {
+// zeroCount++;
+// } else {
+// product *= nums[i];
+// }
+// }
+
+// if (zeroCount > 1) {
+// return solution;
+// }
+
+// if (zeroCount == 1) {
+// for (int i = 0; i < nums.length; i++) {
+// if (nums[i] == 0) {
+// solution[i] = product;
+// return solution;
+// }
+// }
+// }
+
+// for (int i = 0; i < nums.length; i++) {
+// solution[i] = product / nums[i];
+// }
+
+// return solution;
+
+// }
+// }

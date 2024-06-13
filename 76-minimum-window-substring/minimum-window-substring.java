0cbd39a52b9bcf41ffbba6b334932ba122c32bb8
Satwik Bhasin <satwikbhasin@gmail.com>
2024-06-13 17:44:59 -0400

    Time: 21 ms (37.69%) | Memory: 45 MB (47.89%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,54 +1,50 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if (t.length() > s.length() || s.length() == 0)
+        if (s == null || t == null || s.length() < t.length() || s.length() == 0)
             return "";
 
-        HashMap<Character, Integer> windowMap = new HashMap<>();
+        HashMap<Character, Integer> windowFrequency = new HashMap<>();
         HashMap<Character, Integer> tFrequency = new HashMap<>();
 
         for (char c : t.toCharArray()) {
             tFrequency.put(c, tFrequency.getOrDefault(c, 0) + 1);
         }
 
+        int minLength = Integer.MAX_VALUE;
+        int windowStart = -1;
+
         int required = tFrequency.size();
         int satisfied = 0;
-        int validWindowStart = -1;
-
-        int minLength = s.length() + 1;
 
         int left = 0;
-        for (int right = 0; right < s.length(); right++) {
-            char newChar = s.charAt(right);
-            windowMap.put(newChar, windowMap.getOrDefault(newChar, 0) + 1);
+        int right = 0;
 
-            if (tFrequency.containsKey(newChar)
-                    && windowMap.get(newChar).intValue() == tFrequency.get(newChar).intValue()) {
+        while (right < s.length()) {
+            char newChar = s.charAt(right);
+            windowFrequency.put(newChar, windowFrequency.getOrDefault(newChar, 0) + 1);
+
+            if (tFrequency.containsKey(newChar) && windowFrequency.get(newChar).intValue() == tFrequency.get(newChar).intValue()) {
                 satisfied++;
             }
 
             while (left <= right && satisfied >= required) {
-                char leftChar = s.charAt(left);
-
                 if (minLength > right - left + 1) {
                     minLength = right - left + 1;
-                    validWindowStart = left;
+                    windowStart = left;
                 }
+                char leftChar = s.charAt(left);
+                windowFrequency.put(leftChar, windowFrequency.get(leftChar) - 1);
 
-                windowMap.put(leftChar, windowMap.get(leftChar) - 1);
-                if (windowMap.get(leftChar) == 0) {
-                    windowMap.remove(leftChar);
-                }
-
-                if (tFrequency.containsKey(leftChar)
-                        && windowMap.getOrDefault(leftChar, 0) < tFrequency.get(leftChar).intValue()) {
+                if (tFrequency.containsKey(leftChar) && windowFrequency.get(leftChar) < tFrequency.get(leftChar)) {
                     satisfied--;
                 }
 
                 left++;
-
             }
+
+            right++;
         }
 
-        return validWindowStart == -1 ? "" : s.substring(validWindowStart, validWindowStart + minLength);
+        return windowStart == -1 ? "" : s.substring(windowStart, windowStart + minLength);
     }
 }

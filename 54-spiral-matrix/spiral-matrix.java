d23458ac2783cf35bc10d027fdc726d6e8c7ee66
Satwik Bhasin <satwikbhasin@gmail.com>
2024-06-13 19:24:18 -0400

    Time: 0 ms (100.00%) | Memory: 41.4 MB (32.01%) - LeetSync

---
+++ 54-spiral-matrix/spiral-matrix.java
@@ -0,0 +1,38 @@
+class Solution {
+    List<Integer> sol;
+    boolean[][] visited;
+    int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; // right, down, left, up
+
+    private void dfs(int x, int y, int dir, int[][] matrix) {
+        if (x < 0 || x >= matrix.length || y < 0 || y >= matrix[0].length || visited[x][y]) {
+            return;
+        }
+
+        visited[x][y] = true;
+        sol.add(matrix[x][y]);
+
+        int newX = x + directions[dir][0];
+        int newY = y + directions[dir][1];
+
+        if (newX < 0 || newX >= matrix.length || newY < 0 || newY >= matrix[0].length || visited[newX][newY]) {
+            dir = (dir + 1) % 4; // change direction
+            newX = x + directions[dir][0];
+            newY = y + directions[dir][1];
+        }
+
+        dfs(newX, newY, dir, matrix);
+    }
+
+    public List<Integer> spiralOrder(int[][] matrix) {
+        if (matrix.length == 0) {
+            return new ArrayList<>();
+        }
+
+        visited = new boolean[matrix.length][matrix[0].length];
+        sol = new ArrayList<>();
+
+        dfs(0, 0, 0, matrix);
+
+        return sol;
+    }
+}

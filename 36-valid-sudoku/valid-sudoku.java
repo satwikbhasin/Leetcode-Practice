00c2564c08bdcfeaed2a1195ead7f4a2b77a0075
Satwik Bhasin <satwikbhasin@gmail.com>
2024-06-13 18:45:47 -0400

    Time: 2 ms (82.05%) | Memory: 44.8 MB (26.64%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -4,16 +4,24 @@ class Solution {
             HashSet<Character> rowSet = new HashSet<>();
             HashSet<Character> colSet = new HashSet<>();
             HashSet<Character> subgridSet = new HashSet<>();
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] != '.' && !rowSet.add(board[i][j])) return false;
 
-                if (board[j][i] != '.' && !colSet.add(board[j][i])) return false;  
-                
-                int rowIndex = 3 * (i / 3) + (j / 3);
-                int colIndex = 3 * (i % 3) + (j % 3);
-                if (board[rowIndex][colIndex] != '.' && !subgridSet.add(board[rowIndex][colIndex])) return false;
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.' && !rowSet.add(board[i][j]))
+                    return false;
+                if (board[j][i] != '.' && !colSet.add(board[j][i]))
+                    return false;
+            }
+
+            for (int k = 0; k < 3; k++) {
+                for (int l = 0; l < 3; l++) {
+                    int rowIndex = 3 * (i / 3) + k;
+                    int colIndex = 3 * (i % 3) + l;
+                    if (board[rowIndex][colIndex] != '.' && !subgridSet.add(board[rowIndex][colIndex]))
+                        return false;
+                }
             }
         }
+
         return true;
     }
 }

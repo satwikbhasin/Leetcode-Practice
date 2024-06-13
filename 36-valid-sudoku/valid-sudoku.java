1a98d40acb5b0e6cb16693c46b37d0a275e98b1f
Satwik Bhasin <satwikbhasin@gmail.com>
2024-06-13 18:28:23 -0400

    Time: 2 ms (82.05%) | Memory: 44.4 MB (55.58%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,49 +1,20 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        HashSet<Integer> set = new HashSet<>();
-        for (char[] row : board) {
-            set.clear();
-            for (char cell : row) {
-                if (cell != '.') {
-                    int val = cell - '0';
-                    if (val < 1 || val > 9 || set.contains(val)) {
-                        return false;
-                    }
-                    set.add(val);
-                }
-            }
-        }
-
         for (int i = 0; i < 9; i++) {
-            set.clear();
+            HashSet<Character> rowSet = new HashSet<>();
+            HashSet<Character> colSet = new HashSet<>();
+            HashSet<Character> subgridSet = new HashSet<>();
             for (int j = 0; j < 9; j++) {
-                if (board[j][i] != '.') {
-                    int val = board[j][i] - '0';
-                    if (val < 1 || val > 9 || set.contains(val)) {
-                        return false;
-                    }
-                    set.add(val);
-                }
+                // Check row
+                if (board[i][j] != '.' && !rowSet.add(board[i][j])) return false;
+                // Check column
+                if (board[j][i] != '.' && !colSet.add(board[j][i])) return false;
+                // Check subgrid
+                int rowIndex = 3 * (i / 3) + j / 3;
+                int colIndex = 3 * (i % 3) + j % 3;
+                if (board[rowIndex][colIndex] != '.' && !subgridSet.add(board[rowIndex][colIndex])) return false;
             }
         }
-
-        for (int block = 0; block < 9; block++) {
-            set.clear();
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    int rowIndex = 3 * (block / 3) + i;
-                    int colIndex = 3 * (block % 3) + j;
-                    if (board[rowIndex][colIndex] != '.') {
-                        int val = board[rowIndex][colIndex] - '0';
-                        if (val < 1 || val > 9 || set.contains(val)) {
-                            return false;
-                        }
-                        set.add(val);
-                    }
-                }
-            }
-        }
-
         return true;
     }
 }

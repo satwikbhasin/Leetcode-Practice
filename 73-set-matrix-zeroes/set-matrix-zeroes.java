0c4e0cc6d4697fc75ffe7f4f01c56b50a40b0db2
Satwik Bhasin <satwikbhasin@gmail.com>
2024-06-13 20:16:10 -0400

    Time: 1 ms (77.76%) | Memory: 45.9 MB (5.66%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -54,32 +54,3 @@ class Solution {
         }
     }
 }
-
-
-// O(m * n) Time, O(m + n) Space
-// class Solution {
-// public void setZeroes(int[][] matrix) {
-// Set<Integer> zeroRows = new HashSet<>();
-// Set<Integer> zeroCols = new HashSet<>();
-
-// // find all rows and columns which have 0's -> O(m * n)
-// for (int i = 0; i < matrix.length; i++) {
-// for (int j = 0; j < matrix[0].length; j++) {
-// if (matrix[i][j] == 0) {
-// zeroRows.add(i);
-// zeroCols.add(j);
-// }
-// }
-// }
-
-// // Set all cells whose row or col is marked to have 0 -> O(m * n)
-// for (int i = 0; i < matrix.length; i++) {
-// for (int j = 0; j < matrix[0].length; j++) {
-// if (zeroRows.contains(i) || zeroCols.contains(j)) {
-// matrix[i][j] = 0;
-// }
-// }
-// }
-
-// }
-// }

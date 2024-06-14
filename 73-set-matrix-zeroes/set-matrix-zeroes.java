6e48bfac52e717058d6411a874488e2a1467e123
Satwik Bhasin <satwikbhasin@gmail.com>
2024-06-13 20:08:36 -0400

    Time: 0 ms (100.00%) | Memory: 45.7 MB (20.11%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,25 +1,85 @@
+// O(m * n) Time, O(1) Space
 class Solution {
     public void setZeroes(int[][] matrix) {
-        Set<Integer> zeroRows = new HashSet<>();
-        Set<Integer> zeroCols = new HashSet<>();
+        boolean firstRowZero = false;
+        boolean firstColZero = false;
 
-        // find all rows and columns which have 0's -> O(m * n)
+        // Check if first row has zero
+        for (int j = 0; j < matrix[0].length; j++) {
+            if (matrix[0][j] == 0) {
+                firstRowZero = true;
+                break;
+            }
+        }
+
+        // Check if first column has zero
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
+            if (matrix[i][0] == 0) {
+                firstColZero = true;
+                break;
+            }
+        }
+
+        // Use first row and first column to mark zero cells
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
                 if (matrix[i][j] == 0) {
-                    zeroRows.add(i);
-                    zeroCols.add(j);
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (zeroRows.contains(i) || zeroCols.contains(j)) {
+        // Set zero cells based on marks in first row and first column
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
             }
         }
 
+        // Set first row to zero if necessary
+        if (firstRowZero) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // Set first column to zero if necessary
+        if (firstColZero) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
     }
 }
+
+
+// O(m * n) Time, O(m + n) Space
+// class Solution {
+// public void setZeroes(int[][] matrix) {
+// Set<Integer> zeroRows = new HashSet<>();
+// Set<Integer> zeroCols = new HashSet<>();
+
+// // find all rows and columns which have 0's -> O(m * n)
+// for (int i = 0; i < matrix.length; i++) {
+// for (int j = 0; j < matrix[0].length; j++) {
+// if (matrix[i][j] == 0) {
+// zeroRows.add(i);
+// zeroCols.add(j);
+// }
+// }
+// }
+
+// // Set all cells whose row or col is marked to have 0 -> O(m * n)
+// for (int i = 0; i < matrix.length; i++) {
+// for (int j = 0; j < matrix[0].length; j++) {
+// if (zeroRows.contains(i) || zeroCols.contains(j)) {
+// matrix[i][j] = 0;
+// }
+// }
+// }
+
+// }
+// }

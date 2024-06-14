ad7c59ae71200d598c1f0a1da2224d2115244405
Satwik Bhasin <satwikbhasin@gmail.com>
2024-06-14 19:48:49 -0400

    Time: 125 ms (9.05%) | Memory: 61.5 MB (34.94%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
@@ -9,6 +9,21 @@
 //             pair.add(y);
 //             this.sum = x + y;
 //         }
+
+//         @Override
+//         public boolean equals(Object o) {
+//             if (this == o)
+//                 return true;
+//             if (o == null || getClass() != o.getClass())
+//                 return false;
+//             Pair pair1 = (Pair) o;
+//             return pair.equals(pair1.pair);
+//         }
+
+//         @Override
+//         public int hashCode() {
+//             return Objects.hash(pair);
+//         }
 //     }
 
 //     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
@@ -16,54 +31,73 @@
 //         PriorityQueue<Pair> pairPQ = new PriorityQueue<>((a, b) -> (b.sum - a.sum));
 //         for (int i = 0; i < nums1.length; i++) {
 //             for (int j = 0; j < nums2.length; j++) {
-//                 pairPQ.offer(new Pair(nums1[i], nums2[j]));
-//                 if (pairPQ.size() > k) {
-//                     pairPQ.poll();
-//                 }
+//                 Pair curr = new Pair(nums1[i], nums2[j]);
+//                 pairPQ.offer(curr);
+//                     pairCount.put(curr, 1);
+//                     if (pairPQ.size() > k) {
+//                         pairPQ.poll();
+//                     }
+
 //             }
 //         }
 
 //         List<List<Integer>> sol = new ArrayList<>();
 //         int added = 0;
 //         while (!pairPQ.isEmpty()) {
-//             sol.add(pairPQ.poll().pair);
-//             added++;
+//             Pair curr = pairPQ.poll();
+//                 sol.add(curr.pair);
+//                 added++;
+//             }
 //         }
 
-//         return sol;
-//     }
-// }
+//     return sol;
+// }}
+
+import java.util.*;
 
 class Solution {
+    class Pair {
+        List<Integer> pair;
+        int sum;
+
+        public Pair(int x, int y) {
+            pair = new ArrayList<>();
+            pair.add(x);
+            pair.add(y);
+            this.sum = x + y;
+        }
+    }
+
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        int m = nums1.length;
-        int n = nums2.length;
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums1.length == 0 || nums2.length == 0 || k == 0) {
+            return result;
+        }
 
-        List<List<Integer>> ans = new ArrayList<>();
-        Set<Pair<Integer, Integer>> visited = new HashSet<>();
+        PriorityQueue<Pair> maxHeap = new PriorityQueue<>((a, b) -> b.sum - a.sum);
 
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b)->(a[0] - b[0]));
-        minHeap.offer(new int[]{nums1[0] + nums2[0], 0, 0});
-        visited.add(new Pair<Integer, Integer>(0, 0));
-
-        while (k-- > 0 && !minHeap.isEmpty()) {
-            int[] top = minHeap.poll();
-            int i = top[1];
-            int j = top[2];
-
-            ans.add(List.of(nums1[i], nums2[j]));
-
-            if (i + 1 < m && !visited.contains(new Pair<Integer, Integer>(i + 1, j))) {
-                minHeap.offer(new int[]{nums1[i + 1] + nums2[j], i + 1, j});
-                visited.add(new Pair<Integer, Integer>(i + 1, j));
-            }
-
-            if (j + 1 < n && !visited.contains(new Pair<Integer, Integer>(i, j + 1))) {
-                minHeap.offer(new int[]{nums1[i] + nums2[j + 1], i, j + 1});
-                visited.add(new Pair<Integer, Integer>(i, j + 1));
+        // Generate and maintain k smallest pairs using two pointers
+        for (int i = 0; i < Math.min(nums1.length, k); i++) {
+            for (int j = 0; j < Math.min(nums2.length, k); j++) {
+                Pair currPair = new Pair(nums1[i], nums2[j]);
+                // Add current pair to maxHeap if its sum is smaller than the largest sum in
+                // maxHeap or maxHeap size is less than k
+                if (maxHeap.size() < k || currPair.sum < maxHeap.peek().sum) {
+                    maxHeap.offer(currPair);
+                    if (maxHeap.size() > k) {
+                        maxHeap.poll(); // Remove the pair with the largest sum
+                    }
+                } else {
+                    break; // No need to check further pairs with larger sums
+                }
             }
         }
 
-        return ans;
+        // Add k smallest pairs to the result list
+        while (!maxHeap.isEmpty()) {
+            result.add(0, maxHeap.poll().pair); // Add pairs at the beginning to maintain order
+        }
+
+        return result;
     }
 }

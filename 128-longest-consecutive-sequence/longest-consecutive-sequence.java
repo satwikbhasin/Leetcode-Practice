92a956bf93b46cb5b14703bd6065f03748c8a269
Satwik Bhasin <satwikbhasin@gmail.com>
2024-06-14 14:26:12 -0400

    Time: 1060 ms (27.53%) | Memory: 66.6 MB (41.54%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,32 +1,64 @@
+// O(n) Time, O(n) Space
 class Solution {
     public int longestConsecutive(int[] nums) {
         if (nums == null || nums.length == 0)
             return 0;
 
-        Arrays.sort(nums);
+        HashSet<Integer> nums_set = new HashSet<>();
 
-        int longestSeq = 1;
-        int currStreak = 1;
-        boolean streakStarted = false;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != nums[i - 1]) { // this line is added to handle duplicates
-
-                boolean streakMaker = nums[i] - nums[i - 1] == 1 || nums[i] == nums[i - 1];
-                if (streakMaker && !streakStarted) { // start streak
-                    streakStarted = true;
-                    currStreak = 2;
-                } else if (streakMaker && streakStarted) { // streak going on
-                    currStreak++;
-                } else if (!streakMaker && streakStarted) { // break streak
-                    longestSeq = Math.max(longestSeq, currStreak);
-                    streakStarted = false;
-                    currStreak = 1;
-                }
-            }
+        for (int num : nums) {
+            nums_set.add(num);
         }
 
-        longestSeq = Math.max(longestSeq, currStreak);
+        int longestSeq = 1;
+        for (int num : nums) {
+            if (!nums_set.contains(num - 1)) {
+                int currStreak = 1;
+                int currNum = num + 1;
+
+                while (nums_set.contains(currNum)) {
+                    currStreak++;
+                    currNum++;
+                }
+
+                longestSeq = Math.max(longestSeq, currStreak);
+
+            }
+        }
 
         return longestSeq;
     }
 }
+
+// O(n * logn) Time, O(log n) Space
+// class Solution {
+// public int longestConsecutive(int[] nums) {
+// if (nums == null || nums.length == 0)
+// return 0;
+
+// Arrays.sort(nums);
+
+// int longestSeq = 1;
+// int currStreak = 1;
+// boolean streakStarted = false;
+// for (int i = 1; i < nums.length; i++) {
+// if (nums[i] != nums[i - 1]) { // handle duplicates
+// boolean streakMaker = nums[i] - nums[i - 1] == 1 || nums[i] == nums[i - 1];
+// if (streakMaker && !streakStarted) { // start streak
+// streakStarted = true;
+// currStreak = 2;
+// } else if (streakMaker && streakStarted) { // streak going on
+// currStreak++;
+// } else if (!streakMaker && streakStarted) { // break streak
+// longestSeq = Math.max(longestSeq, currStreak);
+// streakStarted = false;
+// currStreak = 1;
+// }
+// }
+// }
+
+// longestSeq = Math.max(longestSeq, currStreak);
+
+// return longestSeq;
+// }
+// }

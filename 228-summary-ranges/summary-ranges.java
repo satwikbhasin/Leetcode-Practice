11f1ed1583e3431f5b0eeea97e974512c4a8d861
Satwik Bhasin <satwikbhasin@gmail.com>
2024-06-14 15:06:46 -0400

    Time: 0 ms (100.00%) | Memory: 41.4 MB (83.39%) - LeetSync

--- 228-summary-ranges/summary-ranges.java
+++ 228-summary-ranges/summary-ranges.java
@@ -1,4 +1,14 @@
 class Solution {
+    private void addRangeToSolution(int start, int end, List<String> sol) {
+        StringBuilder range = new StringBuilder();
+        range.append(start);
+        if (start != end) {
+            range.append("->");
+            range.append(end);
+        }
+        sol.add(range.toString());
+    }
+
     public List<String> summaryRanges(int[] nums) {
         List<String> sol = new ArrayList<>();
 
@@ -7,32 +17,17 @@ class Solution {
 
         int rangeStart = nums[0];
         int rangeEnd = nums[0];
-        StringBuilder range = new StringBuilder();
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] - nums[i - 1] == 1) {
                 rangeEnd = nums[i];
             } else {
-                range.append(rangeStart);
-                if (rangeStart < rangeEnd) {
-                    range.append("->");
-                    range.append(rangeEnd);
-                }
-                sol.add(range.toString());
-                range.setLength(0);
+                addRangeToSolution(rangeStart, rangeEnd, sol);
                 rangeStart = nums[i];
                 rangeEnd = nums[i];
             }
         }
 
-        if (rangeStart == rangeEnd) {
-            range.append(rangeStart);
-            sol.add(range.toString());
-        } else {
-            range.append(rangeStart);
-                range.append("->");
-                range.append(rangeEnd);
-            sol.add(range.toString());
-        }
+        addRangeToSolution(rangeStart, rangeEnd, sol);
 
         return sol;
 

38a3de2b8f71375a8da9b5d021010e86b2ca2c9b
Satwik Bhasin <satwikbhasin@gmail.com>
2024-06-17 18:21:28 -0400

    Time: 5 ms (5.34%) | Memory: 44.8 MB (39.70%) - LeetSync

--- 337-house-robber-iii/house-robber-iii.java
+++ 337-house-robber-iii/house-robber-iii.java
@@ -1,25 +1,49 @@
 // O(n) Time, O(n) Space
 class Solution {
-    private Map<String, Integer> memo;
+    class NodeKey {
+        TreeNode node;
+        boolean isIncluded;
+
+        public NodeKey(TreeNode node, boolean isIncluded) {
+            this.node = node;
+            this.isIncluded = isIncluded;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o)
+                return true;
+            if (o == null || getClass() != o.getClass())
+                return false;
+            NodeKey nodeKey = (NodeKey) o;
+            return isIncluded == nodeKey.isIncluded && Objects.equals(node, nodeKey.node);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(node, isIncluded);
+        }
+    }
+
+    private Map<NodeKey, Integer> memo;
 
     private int dfs(TreeNode node, boolean include) {
         if (node == null) {
             return 0;
         }
 
-        String key = node + "#" + include;
+        NodeKey key = new NodeKey(node, include);
         if (memo.containsKey(key)) {
             return memo.get(key);
         }
 
-        int profit;
+        int profit = 0;
         if (include) {
             int profitIncludingCurr = node.val + dfs(node.left, false) + dfs(node.right, false);
             int profitExcludingCurr = dfs(node.left, true) + dfs(node.right, true);
             profit = Math.max(profitIncludingCurr, profitExcludingCurr);
         } else {
-            int profitExcludingCurr = dfs(node.left, true) + dfs(node.right, true);
-            profit = profitExcludingCurr;
+            profit = dfs(node.left, true) + dfs(node.right, true);
         }
 
         memo.put(key, profit);

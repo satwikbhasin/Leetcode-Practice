71f7952200a5ba5e843db4f45bca65310cc7b8cd
Satwik Bhasin <satwikbhasin@gmail.com>
2024-06-17 19:05:51 -0400

    Time: 30 ms (18.41%) | Memory: 64 MB (20.08%) - LeetSync

--- 2690-house-robber-iv/house-robber-iv.java
+++ 2690-house-robber-iv/house-robber-iv.java
@@ -1,4 +1,5 @@
 class Solution {
+
     private boolean isPossible(int[] nums, int k, int possibleCapability) {
         int[] dp = new int[nums.length + 2];
         for (int i = nums.length - 1; i >= 0; i--) {
@@ -7,16 +8,16 @@ class Solution {
             } else {
                 dp[i] = Math.max(dp[i + 2] + 1, dp[i + 1]);
             }
+
         }
         return dp[0] >= k;
     }
 
     public int minCapability(int[] nums, int k) {
-        // Initialize minCapability and maxCapability
+
         int minCapability = Integer.MAX_VALUE;
         int maxCapability = Integer.MIN_VALUE;
 
-        // Find the minimum and maximum capability values in nums
         for (int num : nums) {
             if (num < minCapability)
                 minCapability = num;
@@ -24,21 +25,13 @@ class Solution {
                 maxCapability = num;
         }
 
-        // Initialize min
         int min = Integer.MAX_VALUE;
-        
-        // Perform binary search to find the minimum capability
         while (minCapability <= maxCapability) {
-            // Calculate midCapability
             int midCapability = minCapability + (maxCapability - minCapability) / 2;
-            
-            // Check if it's possible to rob at least k houses with midCapability
             if (isPossible(nums, k, midCapability)) {
-                // Update min and adjust search range
                 min = midCapability;
                 maxCapability = midCapability - 1;
             } else {
-                // Adjust search range
                 minCapability = midCapability + 1;
             }
         }

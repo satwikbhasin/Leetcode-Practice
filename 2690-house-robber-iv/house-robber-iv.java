14f14b81d97672e75ff58c0405b37298d2e504d4
Satwik Bhasin <satwikbhasin@gmail.com>
2024-06-17 19:19:50 -0400

    Time: 132 ms (5.02%) | Memory: 68.9 MB (5.86%) - LeetSync

--- 2690-house-robber-iv/house-robber-iv.java
+++ 2690-house-robber-iv/house-robber-iv.java
@@ -1,34 +1,22 @@
 class Solution {
 
     private int robHouses(int[] nums, int pos, int k, int possibleCapability, int[] dp) {
-        // Base case: if we have robbed k houses consecutively
         if (k == 0) {
             return 0;
         }
 
-        if(pos >= nums.length){
-            return 0;
+        if (pos >= nums.length) {
+            return Integer.MIN_VALUE;
         }
 
         if (dp[pos] != -1) {
             return dp[pos];
         }
 
-        // Base case: if we reached the end of the array
-        if (pos >= nums.length) {
-            return Integer.MIN_VALUE; // Return a negative value indicating that it's not possible to rob k houses
-        }
-
-        // If the current house exceeds the possible capability, skip it
         if (nums[pos] > possibleCapability) {
             return robHouses(nums, pos + 1, k, possibleCapability, dp);
         }
 
-        // Recursive case:
-        // 1. Rob the current house and continue robbing k - 1 houses starting from two
-        // positions ahead
-        // 2. Skip the current house and continue robbing k houses starting from the
-        // next position
         dp[pos] = Math.max(
                 1 + robHouses(nums, pos + 2, k - 1, possibleCapability, dp),
                 robHouses(nums, pos + 1, k, possibleCapability, dp));

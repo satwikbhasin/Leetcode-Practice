1c21a382d90ce8c36c044e9b58e0a3a9a78d9f8e
Satwik Bhasin <satwikbhasin@gmail.com>
2024-06-17 15:25:18 -0400

    Time: 34 ms (19.32%) | Memory: 44.3 MB (59.56%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,22 +1,60 @@
+// O(n * amount) Time, O(n) Space, TOP-DOWN DP
 class Solution {
+    private int dp(int[] coins, int remainder, int[] dp) {
+        if (remainder < 0) {
+            return -1;
+        }
+
+        if (remainder == 0) {
+            return 0;
+        }
+
+        if(dp[remainder - 1] != 0){
+            return dp[remainder - 1];
+        }
+
+        int min = Integer.MAX_VALUE;
+        for(int coin : coins){
+            int res = dp(coins, remainder - coin, dp);
+            if(res >= 0 && res < min){
+                min = 1 + res;
+            }
+        }
+
+        dp[remainder - 1] = min == Integer.MAX_VALUE ? -1 : min;
+
+        return dp[remainder - 1];
+    }
+
     public int coinChange(int[] coins, int amount) {
         if (amount < 1) {
             return 0;
         }
 
-        int dp[] = new int[amount + 1];
-
-        Arrays.fill(dp, Integer.MAX_VALUE);
-
-        dp[0] = 0;
-        for (int i = 1; i <= amount; i++) {
-            for (int j = 0; j < coins.length; j++) {
-                if (coins[j] <= i && dp[i - coins[j]] != Integer.MAX_VALUE) {
-                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
-                }
-            }
-        }
-
-        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+        return dp(coins, amount, new int[amount]);
     }
 }
+
+// O(n * amount) Time, O(n) Space, BOTTOM-UP DP
+// class Solution {
+// public int coinChange(int[] coins, int amount) {
+// if (amount < 1) {
+// return 0;
+// }
+
+// int dp[] = new int[amount + 1];
+
+// Arrays.fill(dp, Integer.MAX_VALUE);
+
+// dp[0] = 0;
+// for (int i = 1; i <= amount; i++) {
+// for (int j = 0; j < coins.length; j++) {
+// if (coins[j] <= i && dp[i - coins[j]] != Integer.MAX_VALUE) {
+// dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+// }
+// }
+// }
+
+// return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+// }
+// }

f3656b047f5138af3c1ba789e75f068282604c84
Satwik Bhasin <satwikbhasin@gmail.com>
2024-06-17 14:57:07 -0400

    Time: 0 ms (100.00%) | Memory: 40.2 MB (38.67%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,31 +1,51 @@
+// O(n) Time, O(n) Space, BOTTOM-UP DP
 class Solution {
-    int[] ways;
-    int totalWays;
-
-    private int dp(int n) {
-        if (n == 0) {
-            return 1;
-        }
-
-        if(n < 0){
-            return 0;
-        }
-
-        if (ways[n] != -1) {
-            return ways[n];
-        }
-
-        int waysFromHere = dp(n - 1) + dp(n - 2);
-
-        ways[n] = waysFromHere;
-
-        return waysFromHere;
-    }
-
     public int climbStairs(int n) {
-        totalWays = 0;
-        ways = new int[n + 1];
-        Arrays.fill(ways, -1);
-        return dp(n);
+        if(n < 2){
+            return n;
+        }
+        
+        int[] ways = new int[n];
+
+        ways[0] = 1;
+        ways[1] = 2;
+        for (int i = 2; i < n; i++) {
+            ways[i] = ways[i - 1] + ways[i - 2];
+        }
+
+        return ways[n - 1];
     }
 }
+
+// O(n) Time, O(n) Space, TOP-DOWN DP
+// class Solution {
+// int[] ways;
+// int totalWays;
+
+// private int dp(int n) {
+// if (n == 0) {
+// return 1;
+// }
+
+// if(n < 0){
+// return 0;
+// }
+
+// if (ways[n] != -1) {
+// return ways[n];
+// }
+
+// int waysFromHere = dp(n - 1) + dp(n - 2);
+
+// ways[n] = waysFromHere;
+
+// return waysFromHere;
+// }
+
+// public int climbStairs(int n) {
+// totalWays = 0;
+// ways = new int[n + 1];
+// Arrays.fill(ways, -1);
+// return dp(n);
+// }
+// }

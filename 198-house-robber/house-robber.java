91510072c8c3d5ec8d5c9f3cdce177c96c4ffe3b
Satwik Bhasin <satwikbhasin@gmail.com>
2024-06-16 20:46:04 -0400

    Time: 0 ms (100.00%) | Memory: 40.8 MB (63.40%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,28 +1,28 @@
 class Solution {
     int[] visited;
 
-    private int dp(int[] nums, int currPos, int currProfit) {
+    private int dp(int[] nums, int currPos) {
         if (currPos >= nums.length) {
-            return currProfit;
+            return 0;
         }
 
         if (visited[currPos] != -1) {
-            return visited[currPos] + currProfit;
+            return visited[currPos];
         }
 
-        int profitIncludingCurr = dp(nums, currPos + 2, currProfit + nums[currPos]);
-        int profitExcludingCurr = dp(nums, currPos + 1, currProfit);
+        int profitIncludingCurr = dp(nums, currPos + 2) + nums[currPos];
+        int profitExcludingCurr = dp(nums, currPos + 1);
 
-        int result = Math.max(profitIncludingCurr, profitExcludingCurr);
+        int currProfit = Math.max(profitIncludingCurr, profitExcludingCurr);
 
-        visited[currPos] = result - currProfit;
+        visited[currPos] = currProfit;
 
-        return result;
+        return currProfit;
     }
 
     public int rob(int[] nums) {
         visited = new int[nums.length];
         Arrays.fill(visited, -1);
-        return dp(nums, 0, 0);
+        return dp(nums, 0);
     }
 }

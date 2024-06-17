aaf838f6953bcb7d3e19920f9198d324877c0f14
Satwik Bhasin <satwikbhasin@gmail.com>
2024-06-17 18:06:03 -0400

    Time: 18 ms (5.34%) | Memory: 47.3 MB (6.22%) - LeetSync

---
+++ 337-house-robber-iii/house-robber-iii.java
@@ -0,0 +1,34 @@
+class Solution {
+    private Map<String, Integer> memo;
+
+    private int dfs(TreeNode node, boolean include) {
+        if (node == null) {
+            return 0;
+        }
+
+        // Construct a key based on the node's identity and include/exclude state
+        String key = node + "_" + include;
+        if (memo.containsKey(key)) {
+            return memo.get(key);
+        }
+
+        int profit;
+        if (include) {
+            int profitIncludingCurr = node.val + dfs(node.left, false) + dfs(node.right, false);
+            int profitExcludingCurr = dfs(node.left, true) + dfs(node.right, true);
+            profit = Math.max(profitIncludingCurr, profitExcludingCurr);
+        } else {
+            int profitExcludingCurr = dfs(node.left, true) + dfs(node.right, true);
+            profit = profitExcludingCurr;
+        }
+
+        // Store the result in the memoization hashmap
+        memo.put(key, profit);
+        return profit;
+    }
+
+    public int rob(TreeNode root) {
+        memo = new HashMap<>();
+        return dfs(root, true);
+    }
+}

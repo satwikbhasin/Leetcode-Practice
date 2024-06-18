b3d5c03e71a903014978788888c187a35f5c9f68
Satwik Bhasin <satwikbhasin@gmail.com>
2024-06-18 00:03:20 -0400

    Time: 2 ms (69.67%) | Memory: 45.2 MB (39.09%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,43 +1,18 @@
-// O(n) Time, O(1) Space
-// class Solution {
-//     public int jump(int[] nums) {
-//         int minJumps = 0;
-//         int currentJumpsFarthest = 0;
-//         int currentJumpsEnd = 0;
-
-//         for (int i = 0; i < nums.length - 1; i++) {
-//             currentJumpsFarthest = Math.max(currentJumpsFarthest, i + nums[i]);
-
-//             if (i == currentJumpsEnd) {
-//                 minJumps++;
-//                 currentJumpsEnd = currentJumpsFarthest;
-
-//                 if (currentJumpsEnd >= nums.length - 1)
-//                     break;
-//             }
-//         }
-
-//         return minJumps;
-//     }
-// }
-
-// O(n^2) Time, O(1) Space
 class Solution {
     public int jump(int[] nums) {
-        if(nums.length == 0) return 0;
+        int currentMaxReach = 0;
+        int nextMaxReach = 0;
+        int minJumps = 0;
 
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[0] = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            nextMaxReach = Math.max(nextMaxReach, i + nums[i]);
 
-        for (int i = 1; i < nums.length; i++) {
-            for (int j = 0; j < i; j++) {
-                if (j + nums[j] >= i) {
-                    dp[i] = Math.min(dp[i], dp[j] + 1);
-                }
+            if (i == currentMaxReach) {
+                minJumps++;
+                currentMaxReach = nextMaxReach;
             }
         }
 
-        return dp[nums.length - 1];
+        return minJumps;
     }
 }

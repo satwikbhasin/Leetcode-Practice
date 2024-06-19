e0b2dae5b3bae2b1463a6f142ef680e38dac37b1
Satwik Bhasin <satwikbhasin@gmail.com>
2024-06-18 21:34:31 -0400

    Time: 0 ms (100.00%) | Memory: 42.2 MB (21.30%) - LeetSync

--- 86-partition-list/partition-list.java
+++ 86-partition-list/partition-list.java
@@ -8,33 +8,65 @@
  * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+// O(n) Time, O(1) Space
 class Solution {
     public ListNode partition(ListNode head, int x) {
         if (head == null)
             return null;
-        Queue<ListNode> queue = new LinkedList<>();
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode prev = dummy;
-        ListNode curr = dummy.next;
+        ListNode lessHead = new ListNode(0);
+        ListNode lessTail = lessHead;
+        ListNode geHead = new ListNode(0);
+        ListNode geTail = geHead;
 
+        ListNode curr = head;
         while (curr != null) {
-            if (curr.val >= x) {
-                queue.offer(curr);
+            if (curr.val < x) {
+                lessTail.next = curr;
+                lessTail = curr;
             } else {
-                prev.next = curr;
-                prev = curr;
+                geTail.next = curr;
+                geTail = curr;
             }
             curr = curr.next;
         }
 
-        while (!queue.isEmpty()) {
-            prev.next = queue.poll();
-            prev = prev.next;
-        }
+        lessTail.next = geHead.next;
 
-        prev.next = null;
+        geTail.next = null;
 
-        return dummy.next;
+        return lessHead.next;
     }
 }
+
+// O(n) Time, O(n) Space
+// class Solution {
+// public ListNode partition(ListNode head, int x) {
+// if (head == null)
+// return null;
+// Queue<ListNode> queue = new LinkedList<>();
+// ListNode dummy = new ListNode(0);
+// dummy.next = head;
+// ListNode prev = dummy;
+// ListNode curr = dummy.next;
+
+// while (curr != null) {
+// if (curr.val >= x) {
+// queue.offer(curr);
+// } else {
+// prev.next = curr;
+// prev = curr;
+// }
+// curr = curr.next;
+// }
+
+// while (!queue.isEmpty()) {
+// prev.next = queue.poll();
+// prev = prev.next;
+// }
+
+// prev.next = null;
+
+// return dummy.next;
+// }
+// }

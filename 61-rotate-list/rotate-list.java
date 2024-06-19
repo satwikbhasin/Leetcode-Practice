fa801ab47968f72f90ffd541daefec818a6e8872
Satwik Bhasin <satwikbhasin@gmail.com>
2024-06-19 18:53:03 -0400

    Time: 8 ms (5.97%) | Memory: 42.6 MB (26.41%) - LeetSync

---
+++ 61-rotate-list/rotate-list.java
@@ -0,0 +1,45 @@
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null || k == 0)
+            return head;
+
+        ListNode count = head;
+        int totalNodes = 0;
+
+        while (count != null) {
+            totalNodes++;
+
+            // reached the end
+            if (count.next == null) {
+                if (k % totalNodes == 0)
+                    return head;
+                count.next = head;
+                break;
+            }
+
+            count = count.next;
+        }
+
+        System.out.println("Total nodes: " + totalNodes);
+        int newHeadPos = totalNodes - (k % totalNodes);
+        System.out.println("New head position: " + newHeadPos);
+
+        ListNode afterHead = head;
+        ListNode afterTail = afterHead;
+
+        for (int i = 1; i < newHeadPos; i++) {
+            afterTail = afterTail.next;
+        }
+
+        ListNode beforeHead = afterTail.next;
+        ListNode beforeTail = beforeHead;
+        afterTail.next = null;
+
+        for (int i = 0; i < totalNodes - newHeadPos - 1; i++) {
+            beforeTail = beforeTail.next;
+        }
+        beforeTail.next = afterHead;
+
+        return beforeHead;
+    }
+}

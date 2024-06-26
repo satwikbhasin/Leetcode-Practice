f17113e6a09a20cb46b7b1df78125f545d3ec390
Satwik Bhasin <satwikbhasin@gmail.com>
2024-06-26 00:57:02 -0400

    Time: 1 ms (52.34%) | Memory: 42.8 MB (12.93%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,25 +1,19 @@
 class Solution {
     List<List<Integer>> result = new ArrayList<>();
 
-    private void dfsBacktrack(int[] nums, int currIndex, List<Integer> currList, int currLimit) {
-        if (currList.size() == currLimit) {
-            result.add(new ArrayList<>(currList));
-            return;
-        }
+    private void dfsBacktrack(int[] nums, int currIndex, List<Integer> currList) {
+        result.add(new ArrayList<>(currList));
 
         for (int i = currIndex; i < nums.length; i++) {
             currList.add(nums[i]);
-            dfsBacktrack(nums, i + 1, currList, currLimit);
+            dfsBacktrack(nums, i + 1, currList);
             currList.remove(currList.size() - 1);
         }
 
     }
 
     public List<List<Integer>> subsets(int[] nums) {
-        result.add(new ArrayList<>());
-        for (int i = 1; i <= nums.length; i++) {
-            dfsBacktrack(nums, 0, new ArrayList<Integer>(), i);
-        }
+        dfsBacktrack(nums, 0, new ArrayList<Integer>());
         return result;
     }
 

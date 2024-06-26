837be2c24a6e58cc9d0735e06cbe56fc450e0554
Satwik Bhasin <satwikbhasin@gmail.com>
2024-06-26 17:46:33 -0400

    Time: 3 ms (57.54%) | Memory: 43.1 MB (10.77%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.java
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.java
@@ -11,14 +11,17 @@ class Solution {
         }
     }
 
-    TreeMap<Integer, List<Node>> map = new TreeMap<>();
+    HashMap<Integer, List<Node>> map = new HashMap<>();
 
     // O(n * log k) Time because it runs for each node -> O(n)
     // & for adding to treeMap -> (log k)
-    private void dfs(TreeNode node, int verticalLevel, int depth) {
+    private int[] dfs(TreeNode node, int verticalLevel, int depth, int[] minMax) {
         if (node == null) {
-            return;
+            return minMax;
         }
+
+        minMax[0] = Math.min(minMax[0], verticalLevel);
+        minMax[1] = Math.max(minMax[1], verticalLevel);
 
         // create a new node out of the current node
         Node newNode = new Node(depth, node.val);
@@ -27,8 +30,10 @@ class Solution {
         map.computeIfAbsent(verticalLevel, k -> new ArrayList<>()).add(newNode);
 
         // Recursively traverse left and right subtrees
-        dfs(node.left, verticalLevel - 1, depth + 1);
-        dfs(node.right, verticalLevel + 1, depth + 1);
+        dfs(node.left, verticalLevel - 1, depth + 1, minMax);
+        dfs(node.right, verticalLevel + 1, depth + 1, minMax);
+
+        return minMax;
     }
 
     public List<List<Integer>> verticalOrder(TreeNode root) {
@@ -37,15 +42,16 @@ class Solution {
         }
 
         // O(n * log n)
-        dfs(root, 0, 0);
+        int[] minMax = dfs(root, 0, 0, new int[] { 0, 0 });
 
         // O(n log n)
         // Convert TreeMap to a list of list by traversing each level and sorting the
         // list at each level according to depth
         List<List<Integer>> result = new ArrayList<>();
-        for (List<Node> currLevel : map.values()) {
-            Collections.sort(currLevel, (a, b) -> (a.depth - b.depth));
+        for (int i = minMax[0]; i <= minMax[1]; i++) {
             List<Integer> temp = new ArrayList<>();
+            List<Node> currLevel = map.get(i);
+            Collections.sort(currLevel, (a, b) -> (a.depth - b.depth));
             for (Node node : currLevel) {
                 temp.add(node.val);
             }

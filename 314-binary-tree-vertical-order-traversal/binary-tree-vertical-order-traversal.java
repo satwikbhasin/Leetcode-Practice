564e1fed43568f08e3f9e782947bfe8c663ef707
Satwik Bhasin <satwikbhasin@gmail.com>
2024-06-26 18:06:49 -0400

    Time: 2 ms (97.96%) | Memory: 43 MB (17.54%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.java
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.java
@@ -1,61 +1,53 @@
-// O(n * log n) Time, O(n) Space wheren is the total number of nodes
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
 class Solution {
-
-    class Node {
-        int depth;
-        int val;
-
-        public Node(int depth, int val) {
-            this.depth = depth;
-            this.val = val;
-        }
-    }
-
-    HashMap<Integer, List<Node>> map = new HashMap<>();
-
-    // O(n * log k) Time because it runs for each node -> O(n)
-    // & for adding to treeMap -> (log k)
-    private int[] dfs(TreeNode node, int verticalLevel, int depth, int[] minMax) {
-        if (node == null) {
-            return minMax;
-        }
-
-        minMax[0] = Math.min(minMax[0], verticalLevel);
-        minMax[1] = Math.max(minMax[1], verticalLevel);
-
-        // create a new node out of the current node
-        Node newNode = new Node(depth, node.val);
-
-        // Add current node's value to the list of its vertical level
-        map.computeIfAbsent(verticalLevel, k -> new ArrayList<>()).add(newNode);
-
-        // Recursively traverse left and right subtrees
-        dfs(node.left, verticalLevel - 1, depth + 1, minMax);
-        dfs(node.right, verticalLevel + 1, depth + 1, minMax);
-
-        return minMax;
-    }
-
     public List<List<Integer>> verticalOrder(TreeNode root) {
-        if (root == null) {
-            return new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null)
+            return result;
+
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        Queue<Pair<TreeNode, Integer>> queue = new LinkedList<>();
+
+        int currLevel = 0, minLevel = 0, maxLevel = 0;
+
+        queue.offer(new Pair(root, currLevel));
+
+        while (!queue.isEmpty()) {
+            int breadth = queue.size();
+            for (int i = 0; i < breadth; i++) {
+                Pair<TreeNode, Integer> currPair = queue.poll();
+                root = currPair.getKey();
+                currLevel = currPair.getValue();
+                map.computeIfAbsent(currLevel, k -> new ArrayList<>()).add(root.val);
+
+                minLevel = Math.min(minLevel, currLevel);
+                maxLevel = Math.max(maxLevel, currLevel);
+
+                if (root.left != null) {
+                    queue.offer(new Pair(root.left, currLevel - 1));
+                }
+                if (root.right != null) {
+                    queue.offer(new Pair(root.right, currLevel + 1));
+                }
+            }
         }
 
-        // O(n * log n)
-        int[] minMax = dfs(root, 0, 0, new int[] { 0, 0 });
-
-        // O(n log n)
-        // Convert TreeMap to a list of list by traversing each level and sorting the
-        // list at each level according to depth
-        List<List<Integer>> result = new ArrayList<>();
-        for (int i = minMax[0]; i <= minMax[1]; i++) {
-            List<Integer> temp = new ArrayList<>();
-            List<Node> currLevel = map.get(i);
-            Collections.sort(currLevel, (a, b) -> (a.depth - b.depth));
-            for (Node node : currLevel) {
-                temp.add(node.val);
-            }
-            result.add(temp);
+        for (int i = minLevel; i <= maxLevel; i++) {
+            result.add(map.get(i));
         }
 
         return result;

333f860718cb5a6b9657797ab2598dcbd43d8f46
Satwik Bhasin <satwikbhasin@gmail.com>
2024-06-27 19:18:04 -0400

    Time: 18 ms (75.60%) | Memory: 94.4 MB (63.64%) - LeetSync

--- 77-combinations/combinations.java
+++ 77-combinations/combinations.java
@@ -1,31 +1,27 @@
 class Solution {
     List<List<Integer>> solution;
-    HashMap<Integer, HashSet<Integer>> seen;
 
-    // private String
-
-    private void dfs(int range, int reqLength, List<Integer> currList, int currLength, int idx) {
-        if (currLength == reqLength) {
+    private void dfs(int range, int reqLength, List<Integer> currList, int start) {
+        if (currList.size() == reqLength) {
             solution.add(new ArrayList<>(currList));
             return;
         }
 
-        for (int i = idx; i <= range; i++) {
+        for (int i = start; i <= range; i++) {
             currList.add(i);
-            dfs(range, reqLength, currList, currLength + 1, i + 1);
+            dfs(range, reqLength, currList, i + 1);
             currList.remove(currList.size() - 1);
         }
     }
 
     public List<List<Integer>> combine(int n, int k) {
         solution = new ArrayList<>();
-        seen = new HashMap<>();
 
         if (k > n) {
             return solution;
         }
 
-        dfs(n, k, new ArrayList<>(), 0, 1);
+        dfs(n, k, new ArrayList<>(), 1);
 
         return solution;
     }

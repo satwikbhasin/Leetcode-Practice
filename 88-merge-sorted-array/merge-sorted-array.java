d6ae0019c3ad11a69e84bf7599103ff264f18e8b
Satwik Bhasin <satwikbhasin@gmail.com>
2024-06-27 17:52:37 -0400

    Time: 1 ms (33.99%) | Memory: 42.2 MB (29.78%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,23 +1,35 @@
+// O(m + n) Time, O(1) Space
+// class Solution {
+//     public void merge(int[] nums1, int m, int[] nums2, int n) {
+//         int ptr1 = m - 1;
+//         int ptr2 = n - 1;
+//         int resultPtr = m + n - 1;
+
+//         while (ptr1 >= 0 && ptr2 >= 0) {
+//             if (nums1[ptr1] > nums2[ptr2]) {
+//                 nums1[resultPtr--] = nums1[ptr1--];
+//             } else {
+//                 nums1[resultPtr--] = nums2[ptr2--];
+//             }
+//         }
+
+//         while (ptr1 < 0 && ptr2 >= 0) {
+//             nums1[resultPtr--] = nums2[ptr2--];
+//         }
+
+//         while (ptr2 < 0 && ptr1 >= 0) {
+//             nums1[resultPtr--] = nums1[ptr1--];
+//         }
+//     }
+// }
+
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int ptr1 = m - 1;
-        int ptr2 = n - 1;
-        int resultPtr = m + n - 1;
 
-        while (ptr1 >= 0 && ptr2 >= 0) {
-            if (nums1[ptr1] > nums2[ptr2]) {
-                nums1[resultPtr--] = nums1[ptr1--];
-            } else {
-                nums1[resultPtr--] = nums2[ptr2--];
-            }
+        for (int i = m, j = 0; i < m + n && j < n; i++, j++) {
+            nums1[i] = nums2[j];
         }
 
-        while (ptr1 < 0 && ptr2 >= 0) {
-            nums1[resultPtr--] = nums2[ptr2--];
-        }
-
-        while (ptr2 < 0 && ptr1 >= 0) {
-            nums1[resultPtr--] = nums1[ptr1--];
-        }
+        Arrays.sort(nums1);
     }
 }

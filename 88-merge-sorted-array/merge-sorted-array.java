4a2ccbd76448d4785cef5682a6d5048adfa197be
Satwik Bhasin <satwikbhasin@gmail.com>
2024-06-27 17:56:10 -0400

    Time: 0 ms (100.00%) | Memory: 41.7 MB (90.16%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,35 +1,36 @@
 // O(m + n) Time, O(1) Space
-// class Solution {
-//     public void merge(int[] nums1, int m, int[] nums2, int n) {
-//         int ptr1 = m - 1;
-//         int ptr2 = n - 1;
-//         int resultPtr = m + n - 1;
-
-//         while (ptr1 >= 0 && ptr2 >= 0) {
-//             if (nums1[ptr1] > nums2[ptr2]) {
-//                 nums1[resultPtr--] = nums1[ptr1--];
-//             } else {
-//                 nums1[resultPtr--] = nums2[ptr2--];
-//             }
-//         }
-
-//         while (ptr1 < 0 && ptr2 >= 0) {
-//             nums1[resultPtr--] = nums2[ptr2--];
-//         }
-
-//         while (ptr2 < 0 && ptr1 >= 0) {
-//             nums1[resultPtr--] = nums1[ptr1--];
-//         }
-//     }
-// }
-
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int ptr1 = m - 1;
+        int ptr2 = n - 1;
+        int resultPtr = m + n - 1;
 
-        for (int i = m, j = 0; i < m + n && j < n; i++, j++) {
-            nums1[i] = nums2[j];
+        while (ptr1 >= 0 && ptr2 >= 0) {
+            if (nums1[ptr1] > nums2[ptr2]) {
+                nums1[resultPtr--] = nums1[ptr1--];
+            } else {
+                nums1[resultPtr--] = nums2[ptr2--];
+            }
         }
 
-        Arrays.sort(nums1);
+        while (ptr1 < 0 && ptr2 >= 0) {
+            nums1[resultPtr--] = nums2[ptr2--];
+        }
+
+        // while (ptr2 < 0 && ptr1 >= 0) {
+        //     nums1[resultPtr--] = nums1[ptr1--];
+        // }
     }
 }
+
+// O(m + n * log (m + n)) Time, O(log (m + n)) Space
+// class Solution {
+//     public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+//         for (int i = m, j = 0; i < m + n && j < n; i++, j++) {
+//             nums1[i] = nums2[j];
+//         }
+
+//         Arrays.sort(nums1);
+//     }
+// }

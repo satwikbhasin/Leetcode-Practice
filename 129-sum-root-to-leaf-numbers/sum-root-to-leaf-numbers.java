5724f26d2827c18d388c7abac2d80396c275608a
Satwik Bhasin <satwikbhasin@gmail.com>
2024-06-27 00:14:27 -0400

    Time: 1 ms (23.52%) | Memory: 40.9 MB (58.18%) - LeetSync

---
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    int sum;
+
+    private void dfs(TreeNode node, StringBuilder currNum) {
+        if (node == null) {
+            return;
+        }
+        
+        currNum.append(node.val);
+
+        if (node.left == null && node.right == null) {
+            sum += Integer.valueOf(currNum.toString());
+        } else {
+            dfs(node.left, currNum);
+            dfs(node.right, currNum);
+        }
+
+        currNum.setLength(currNum.length() - 1);
+    }
+
+    public int sumNumbers(TreeNode root) {
+        sum = 0;
+        dfs(root, new StringBuilder());
+        return sum;
+    }
+}

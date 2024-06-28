fc78c024c54a668df17e542f8931a75778234caa
Satwik Bhasin <satwikbhasin@gmail.com>
2024-06-27 23:28:02 -0400

    Time: 110 ms (43.67%) | Memory: 45.8 MB (6.09%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,30 +1,34 @@
 class Solution {
     int[] nums;
     int target;
-    int ways;
+    HashMap<String, Integer> memo;
 
     int[] signs = new int[] { 1, -1 };
 
-    private void dfs(int currResult, int idx) {
-        if (idx == nums.length) {
-            if (currResult == target) {
-                ways++;
-            }
-            return;
+    private int dfs(int currResult, int idx) {
+        if (idx == nums.length)
+            return currResult == target ? 1 : 0;
+
+        String key = idx + "," + currResult;
+        if (memo.containsKey(key)) {
+            return memo.get(key);
         }
 
+        int currWays = 0;
         for (int sign : signs) {
-            dfs(currResult + sign * nums[idx], idx + 1);
+            currWays += dfs(currResult + sign * nums[idx], idx + 1);
         }
+
+        memo.put(key, currWays);
+
+        return memo.get(key);
     }
 
     public int findTargetSumWays(int[] nums, int target) {
         this.nums = nums;
         this.target = target;
-        this.ways = 0;
+        this.memo = new HashMap<>();
 
-        dfs(0, 0);
-
-        return ways;
+        return dfs(0, 0);
     }
 }

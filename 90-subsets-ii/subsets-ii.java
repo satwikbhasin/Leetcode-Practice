2b41b53f5ea50368f5252ba7e8efb7a3264afe8b
Satwik Bhasin <satwikbhasin@gmail.com>
2024-06-30 17:10:55 -0400

    Time: 1 ms (99.94%) | Memory: 43.2 MB (81.55%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,13 +1,8 @@
 class Solution {
     List<List<Integer>> result;
-    HashSet<List<Integer>> sets;
 
     private void dfs(int[] nums, int idx, ArrayList<Integer> subset) {
-        if (!sets.contains(subset)) {
-            List<Integer> curr = new ArrayList<>(subset);
-            result.add(curr);
-            sets.add(curr);
-        }
+        result.add(new ArrayList<>(subset));
 
         for (int i = idx; i < nums.length; i++) {
             if (i != idx && nums[i] == nums[i - 1]) {
@@ -22,7 +17,6 @@ class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
         result = new ArrayList<>();
-        sets = new HashSet<>();
         dfs(nums, 0, new ArrayList<Integer>());
         return result;
     }

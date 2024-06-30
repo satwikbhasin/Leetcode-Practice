922fe4726ed67119cd32fccc4d4df55ad6b4e91b
Satwik Bhasin <satwikbhasin@gmail.com>
2024-06-30 14:21:49 -0400

    Time: 35 ms (22.74%) | Memory: 40.5 MB (86.87%) - LeetSync

--- 465-optimal-account-balancing/optimal-account-balancing.java
+++ 465-optimal-account-balancing/optimal-account-balancing.java
@@ -1,59 +1,29 @@
 class Solution {
-    
-    private Map<String, Integer> memo;
-    
+
     private int dfs(List<Integer> debts, int idx) {
-        // Skip settled accounts
         while (idx < debts.size() && debts.get(idx) == 0) {
             idx++;
         }
 
-        // All accounts settled
         if (idx == debts.size()) {
             return 0;
         }
 
-        // Create a key for the current state
-        String key = generateKey(debts, idx);
-        
-        // Check if the result for this state is already computed
-        if (memo.containsKey(key)) {
-            return memo.get(key);
-        }
-
         int transactions = Integer.MAX_VALUE;
-        
-        // Try settling debt at idx with subsequent debts
         for (int i = idx + 1; i < debts.size(); i++) {
             if (debts.get(idx) * debts.get(i) < 0) {
-                // Settle debt[idx] with debt[i]
                 debts.set(i, debts.get(i) + debts.get(idx));
                 transactions = Math.min(transactions, 1 + dfs(debts, idx + 1));
                 debts.set(i, debts.get(i) - debts.get(idx));
             }
         }
 
-        // Store the result in memoization map
-        memo.put(key, transactions);
         return transactions;
     }
 
-    private String generateKey(List<Integer> debts, int idx) {
-        // Generate a key for the current state
-        StringBuilder keyBuilder = new StringBuilder();
-        for (int i = idx; i < debts.size(); i++) {
-            if (debts.get(i) != 0) {
-                keyBuilder.append(debts.get(i)).append(',');
-            }
-        }
-        return keyBuilder.toString();
-    }
-
     public int minTransfers(int[][] transactions) {
-        // Initialize memoization map
-        memo = new HashMap<>();
-        
         HashMap<Integer, Integer> accounts = new HashMap<>();
+
         for (int[] transaction : transactions) {
             int from = transaction[0];
             int to = transaction[1];
@@ -64,6 +34,7 @@ class Solution {
         }
 
         List<Integer> debts = new ArrayList<>();
+
         for (int account : accounts.values()) {
             if (account != 0) {
                 debts.add(account);
@@ -72,4 +43,5 @@ class Solution {
 
         return dfs(debts, 0);
     }
+    
 }
